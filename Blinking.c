d72424d97a893c81c3000a4d6334ddf1711f980b
Manuel Poot <manuel.poot.c@gmail.com>
2023-02-04 10:43:12 -0600

    Updated vim source code

--- Blinking.c
+++ Blinking.c
@@ -2,10 +2,15 @@
 
 int main()
 {
+    printf("Hello Moto")
     if (0 == 1)
     {
-	int var = 3
+	    int var = 3
+	    int var = 3
+	    int var = 3
+	    int var = 3
     }
+    printf("Hello Moto")
     printf("Hello Moto")
     return 0;
 }
